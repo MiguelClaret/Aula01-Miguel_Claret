2fb3bfd0ff449627be0ba4c1433f581f02b2fcc7
MiguelClaret <158833503+MiguelClaret@users.noreply.github.com>
2024-02-06 15:58:11 -0300

    code nota aluno

---
+++ Nota_aluno.js
@@ -0,0 +1,13 @@
+let nota1 = 4;
+let nota2 = 5;
+
+let notaF = (nota1 + nota2)/2;
+
+if(notaF > 6){
+    console.log("Sua Nota Final é:", notaF);
+    console.log("Parabéns, você passou, te vejo ano que vem!");
+}
+else{
+    console.log("Sua Nota Final é:", notaF);
+    console.log("Infelizmente você não passou, te vejo nas férias");
+}
